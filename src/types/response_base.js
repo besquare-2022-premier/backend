972defb283888f6902c2fdeeaf831dc51b95e386
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-10-30 23:35:37 +0900

    api,types,database: draft off the basic structure of the code

---
+++ src/types/response_base.js
@@ -0,0 +1,14 @@
+/**
+ * Base type of all non-paginated responses
+ */
+class ResponseBase {
+  /**
+   * Construct a response
+   * @param {number} status
+   * @param {string} message
+   */
+  constructor(status = 0, message = "OK") {
+    this.status = status;
+    this.message = message;
+  }
+}

3f12a2034e2a555c89e848b73675127b89474020
Amir <amir.zahari@besquare.com.my>
2022-11-02 10:18:19 +0800

    fixed: broken files and added db implementation

--- src/database/PostgresDatabase.js
+++ src/database/PostgresDatabase.js
@@ -14,11 +14,11 @@ class PostgresDatabase extends IDatabase {
      * @type {Pool}
      */
     this.pool = new Pool({
-      host: "localhost",
-      user: "postgres",
-      port: 5432,
-      password: "postgres",
-      database: "premier_project",
+      host: process.env.POSTGRES_SERVER,
+      user: process.env.POSTGRES_USER,
+      port: process.env.POSTGRES_PORT,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
     });
     this.pool.on("error", (err, _client) => {
       console.error("Postgres: Something went wrong!!!!");
@@ -74,32 +74,107 @@ class PostgresDatabase extends IDatabase {
     });
   }
 
+  async voidVerificationCode(code) {
+    await this.#doConnected(async function (client) {
+      await client.query(
+        `DELETE FROM premier.verification
+        WHERE verification_code = $1`,
+        [code]
+      );
+    });
+  }
+
   async obtainUserPasswordHash(id) {
     await this.#doConnected(async function (client) {
       let result = await client.query(
         `SELECT pwd FROM premier.user_details 
-        WHERE username=$1 OR email = $1`,
+        WHERE username=$1 OR email = $1 OR loginid = $1`,
         [id]
       );
       return result.rows[0]?.pwd ?? null;
     });
   }
 
-  async addUser(user, password) {}
+  //need checking
+  async addUser(user, password) {
+    await this.#doConnected(async function (client) {
+      await client.query(
+        `INSERT INTO premier.user_details(
+          loginid, fname, lname, username, email, pwd,
+          tel_no, residence, address, birthday, gender, secure_word
+        )
+        VALUES ($1.loginid, $1.firstname, $1.lastname,
+          $1.username, $1.email, $2, $1.tel_no, $1.residence,
+          $1.address, $1.birthday, $1.gender, $1.secure_word)`,
+        [user, password]
+      );
+    });
+  }
 
-  async getUser(loginid) {}
+  async getUser(loginid) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT * FROM premier.user_details
+        WHERE loginid = $1`,
+        [loginid]
+      );
+      return result.rows[0]?.loginid ?? null;
+    });
+  }
 
-  async updateUserSubtle(loginid, changes) {}
+  //need checking
+  async updateUserSubtle(loginid, changes) {
+    await this.#doConnected(async function (client) {
+      await client.query(
+        `UPDATE premier.user_details
+        SET $2
+        WHERE loginid = $1`,
+        [loginid, changes]
+      );
+    });
+  }
 
   async recordAccessToken(token, loginid) {}
 
   async touchAccessToken(token) {}
 
-  async touchAccessToken(token) {}
+  async revokeAccessToken(token) {}
 
-  async getCategories() {}
+  async getUserSecureWord(id) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT secure_word FROM premier.user_details
+        WHERE username = $1 OR email = $1`,
+        [id]
+      );
+      return result.rows[0]?.secure_word ?? null;
+    });
+  }
 
-  async getProducts(search, offset = 0, limit = 50, randomize = false) {}
+  async getCategories() {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(`SELECT * FROM premier.category`);
+      return result.rows[0] ?? null;
+    });
+  }
+
+  async getProducts(search, offset = 0, limit = 50, randomize = false) {
+    let params = [offset, limit];
+    if (search) params.push(`%${search}%`);
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT * FROM premier.product
+        WHERE (stock >0 or stock =-1) ${
+          search ? "  AND product_name LIKE $3  " : ""
+        }
+        ${randomize ? " ORDER BY RANDOM() " : ""}
+        OFFSET $1
+        LIMIT $2`,
+        params
+      );
+      return result.rows[0] ?? null;
+    });
+  }
 
   async getProductsByCategory(
     category,
@@ -107,21 +182,102 @@ class PostgresDatabase extends IDatabase {
     offset = 0,
     limit = 50,
     randomize = false
-  ) {}
+  ) {
+    let params = [category, offset, limit];
+    if (search) params.push(`%${search}%`);
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT c.category_name, p.*
+        FROM premier.product AS p
+        INNER JOIN premier.category AS c
+	        ON c.categoryid = p.categoryid
+        WHERE c.category_name = $1
+          AND (stock > 0 or stock stock =-1) ${
+            search ? " AND product_name like $4 " : ""
+          }
+          ${randomize ? "ORDERBY RANDOM() " : ""}
+        OFFSET $2
+        LIMIT $3`,
+        params
+      );
+      return result.rows[0] ?? null;
+    });
+  }
 
-  async getOrdersOfUser(loginid) {}
+  async getOrdersOfUser(loginid) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT * from premier.orders
+        WHERE loginid = $1`,
+        [loginid]
+      );
+      return result.rows[0] ?? null;
+    });
+  }
 
-  async getUserOrder(loginid, orderid) {}
+  async getUserOrder(loginid, orderid) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT ud.username, ud.email, ud.tel_no,
+	        od.*, o.ship_address, o.country
+        FROM premier.user_details AS ud
+        INNER JOIN premier.orders AS o
+	        ON ud.loginid = o.loginid
+        INNER JOIN premier.order_details AS od
+	        ON o.orderid = od.orderid
+        WHERE ud.loginid = $1 AND o.orderid = $2`
+      );
+      [loginid, orderid];
+      return result.rows[0] ?? null;
+    });
+  }
 
-  async _expandOrderDetails(order) {}
+  async _expandOrderDetails(order) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT o.*, od.productid, od.quantity, od.price
+        FROM premier.orders AS o
+        INNER JOIN premier.order_details AS od
+	      ON o.orderid = od.orderid`
+      );
+      return result.rows[0] ?? null;
+    });
+  }
 
-  async getUserCart(loginid) {}
+  async getUserCart(loginid) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT o.orderid FROM premier.orders AS o
+        WHERE o.loginid = $1 AND NOT EXISTS
+        (SELECT t.orderid FROM premier.transaction AS t
+          WHERE o.orderid = t.orderid)`
+      );
+      return result.rows[0] ?? null;
+    });
+  }
 
   async updateOrderSubtle(orderid, changes) {}
 
-  async addTransaction(tx) {}
+  // async addTransaction(tx) {
+  //   await this.#doConnected(async function (client) {
+  //     await client.query(
+  //       `INSERT INTO premier.transaction
+  //       VALUES ($1)`,
+  //       [tx]
+  //     );
+  //   });
+  // }
 
-  async getTransaction(loginid, txid) {}
+  async getTransaction(loginid, txid) {
+    await this.#doConnected(async function (client) {
+      let result = await client.query(
+        `SELECT * FROM premier.transaction
+        WHERE loginid = $1 AND transactionid = $2`,
+        [loginid, txid]
+      );
+      return result.rows[0] ?? null;
+    });
+  }
 
   async updateTransactionSubtle(txid, changes) {}
 }

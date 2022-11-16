0d7ef912e89a521a190c4cc8881308c001295e17
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-11-16 14:58:36 +0900

    base: fix wrong mounting

--- src/endpoints/_defs.js
+++ src/endpoints/_defs.js
@@ -9,7 +9,6 @@ app.use("/csrf", require("./csrf"));
 app.use("/product", require("./product"));
 app.use("/whoami", require("./profile"));
 app.use("/orders", require("./order"));
-app.use("/__callback", require("./callback"));
 app.use("/product-review", require("./review"));
 
 module.exports = app;

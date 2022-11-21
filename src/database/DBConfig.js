63578afc4fbebed2a087cc4021911d0cb896b201
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-11-22 00:11:20 +0900

    redis: avoid redis related hangs due to the accidental init of redis

--- src/database/DBConfig.js
+++ src/database/DBConfig.js
@@ -1,11 +1,15 @@
 const IDatabase = require("./IDatabase");
 const RedisCachedDatabase = require("./RedisCachedDatabase");
-// const PostgresDatabase = require("./PostgresDatabase");
+const PostgresDatabase = require("./PostgresDatabase");
 /**
  * This file provide a global exports of the database instance to the rest of the application
  * @type {IDatabase}
  */
-module.exports = new RedisCachedDatabase();
+if (process.env.REDIS_HOST && process.env.REDIS_PASS) {
+  module.exports = new RedisCachedDatabase();
+} else {
+  module.exports = new PostgresDatabase();
+}
 if (!(module.exports instanceof IDatabase)) {
   throw new Error("The database is not an instance of IDatabase");
 }
